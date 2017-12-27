793f7d47419c0f645d1f6bcaf8e5c2dc2182510e
Martin Di Paola <martinp.dipaola@gmail.com>
2017-12-27 14:24:07 +0000

    Implemented a coverage engine

--- r.py
+++ r.py
@@ -1,4 +1,28 @@
-import sys
+import sys, os, contextlib
 from byexample.byexample import main
+
+try:
+    import coverage
+
+    @contextlib.contextmanager
+    def coverage_measure():
+        cov = coverage.Coverage(source=['byexample'], auto_data=True)
+        cov.start()
+        try:
+            yield
+        finally:
+            cov.stop()
+
+except ImportError:
+    @contextlib.contextmanager
+    def coverage_measure():
+        yield
+
 if __name__ == '__main__':
-    sys.exit(main())
+    if os.environ.get('BYEXAMPLE_COVERAGE_TEST'):
+        with coverage_measure():
+            sys.exit(main())
+
+    else:
+        sys.exit(main())
+
