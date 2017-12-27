ae8c24a8794253595600994fe5f6152f6e8d1975
Martin Di Paola <martinp.dipaola@gmail.com>
2017-12-27 21:58:06 +0000

    Updated deps: added tqdm and pygments. Bump version.

--- setup.py
+++ setup.py
@@ -9,14 +9,40 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
+import sys
+
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+# the following are the required dependencies
+# without them, we cannot run byexample
+required_deps=[
+    'pexpect>=4,<5',  # pexpect 4.x.x required
+    ]
+
+# these, on the other hand, are optional nice to have
+# dependencies. we'll install them by default but if they
+# are not present, byexample will run normally.
+nice_to_have_deps=[
+    'tqdm>=4,<5',     # tqdm 4.x.x required
+    'pygments>=2,<3', # pygments 2.x.x required
+    ]
+
+# run
+# python setup.py install --byexample-minimal
+# to install only the required dependencies
+if '--byexample-minimal' in sys.argv:
+    sys.argv.remove('--byexample-minimal')
+    install_deps = required_deps
+
+else:
+    install_deps = required_deps + nice_to_have_deps
+
 setup(
     name='byexample',
-    version='2.1.1',
+    version='3.0.0',
 
     description='Write snippets of code in Python, Ruby, and others as ' +\
                 'documentation and execute them as regression tests.',
@@ -47,11 +73,11 @@ setup(
     ],
 
     python_requires='>=2.6',
-    install_requires=['pexpect>=4,<5'], # pexpect 4.x.x required
+    install_requires=install_deps,
 
     keywords='doctest documentation test testing',
 
-    packages=['byexample', 'byexample.interpreters'],
+    packages=['byexample', 'byexample.modules'],
 
     entry_points={
         'console_scripts': [
