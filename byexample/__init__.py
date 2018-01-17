6364f3e1771445d8149b7c8b45c76c3012aeed5f
Martin Di Paola <martinp.dipaola@gmail.com>
2018-01-17 15:08:50 +0000

    The doc must not contains a new line (pip show breaks)

--- byexample/__init__.py
+++ byexample/__init__.py
@@ -1,4 +1,3 @@
-'''Write snippets of code in C++, Python, Ruby, and others as
-documentation and execute them as regression tests.'''
+'''Write snippets of code in C++, Python, Ruby, and others as documentation and execute them as regression tests.'''
 
 __version__ = "3.1.0"
