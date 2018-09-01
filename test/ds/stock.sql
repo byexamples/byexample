7fdea3cac48beb377e9c570e412bc3b389672ec7
Martin Di Paola <martinp.dipaola@gmail.com>
2018-09-01 13:46:11 +0000

    Stock dummy SQLite database.

---
+++ test/ds/stock.sql
@@ -0,0 +1,9 @@
+# Stock dummy SQLite database.
+# The values were taken from https://docs.python.org/3/library/sqlite3.html
+CREATE TABLE stocks
+(date text, trans text, symbol text, qty real, price real)
+
+INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)
+INSERT INTO stocks VALUES ('2006-03-28', 'BUY', 'IBM', 1000, 45.00)
+INSERT INTO stocks VALUES ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00)
+INSERT INTO stocks VALUES ('2006-04-06', 'SELL', 'IBM', 500, 53.00)
