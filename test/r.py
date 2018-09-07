a1a5ce494ac0569cd3ad68df1f6c93f92921c444
Martin Di Paola <martinp.dipaola@gmail.com>
2018-09-07 03:08:05 +0000

    Fixed sys.path/import messed up in test/r.py (and q.py)

--- test/r.py
+++ test/r.py
@@ -1,27 +1,31 @@
-import sys, os, contextlib
-try:
-    import coverage
-
-    @contextlib.contextmanager
-    def coverage_measure():
-        cov = coverage.Coverage(source=['byexample'], auto_data=True)
-        cov.start()
-        try:
-            yield
-        finally:
-            cov.stop()
-
-except ImportError:
-    @contextlib.contextmanager
-    def coverage_measure():
-        print("Warning: coverage is not installed")
-        yield
+import sys, os
 
 if __name__ == '__main__':
+    home = os.path.join(os.path.dirname(__file__), "../")
+    sys.path.append(home)
+    del sys.path[0]
     if os.environ.get('BYEXAMPLE_COVERAGE_TEST'):
-        with coverage_measure():
-            from byexample.byexample import main
-            sys.exit(main())
+        try:
+            import contextlib
+            from coverage import Coverage
+
+            @contextlib.contextmanager
+            def coverage_measure():
+                cov = Coverage(source=['byexample'], auto_data=True)
+                cov.start()
+                try:
+                    yield
+                finally:
+                    cov.stop()
+
+            with coverage_measure():
+                from byexample.byexample import main
+                sys.exit(main())
+
+        except ImportError as e:
+            print("ImportError:\n%s" % str(e))
+            print("Warning: coverage is not installed -> pip install coverage")
+            sys.exit(-1)
 
     else:
         from byexample.byexample import main
