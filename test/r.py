7120361cc7992c276b989fc912cd8e7b8a0a6a7a
Martin Di Paola <martinp.dipaola@gmail.com>
2018-10-08 18:49:23 +0000

    fix r.py

--- test/r.py
+++ test/r.py
@@ -1,31 +1,27 @@
-import sys, os
+import sys, os, contextlib
+try:
+    import coverage
+
+    @contextlib.contextmanager
+    def coverage_measure():
+        cov = coverage.Coverage(source=['byexample'], auto_data=True)
+        cov.start()
+        try:
+            yield
+        finally:
+            cov.stop()
+
+except ImportError:
+    @contextlib.contextmanager
+    def coverage_measure():
+        print("Warning: coverage is not installed")
+        yield
 
 if __name__ == '__main__':
-    home = os.path.join(os.path.dirname(__file__), "../")
-    sys.path.append(home)
-    del sys.path[0]
     if os.environ.get('BYEXAMPLE_COVERAGE_TEST'):
-        try:
-            import contextlib
-            from coverage import Coverage
-
-            @contextlib.contextmanager
-            def coverage_measure():
-                cov = Coverage(source=['byexample'], auto_data=True)
-                cov.start()
-                try:
-                    yield
-                finally:
-                    cov.stop()
-
-            with coverage_measure():
-                from byexample.byexample import main
-                sys.exit(main())
-
-        except ImportError as e:
-            print("ImportError:\n%s" % str(e))
-            print("Warning: coverage is not installed -> pip install coverage")
-            sys.exit(-1)
+        with coverage_measure():
+            from byexample.byexample import main
+            sys.exit(main())
 
     else:
         from byexample.byexample import main
