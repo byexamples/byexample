02ecf9ec53ff21944c329d8d28465df8d44f2360
Martin Di Paola <martinp.dipaola@gmail.com>
2018-10-20 12:59:43 +0000

    Fixed pyte version (dependency)

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ exec(open(path.join(here, 'byexample', '__init__.py')).read())
 required_deps=[
     'pexpect>=4,<5',    # pexpect 4.x.x required
     'appdirs>=1.4.3<2', # appdirs 1.4.x (x >= 3) required
+    'pyte==0.8.0',       # pyte exact version 0.8.0 required
     ]
 
 # these, on the other hand, are optional nice to have
