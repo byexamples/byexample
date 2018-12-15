c0d6e05cfc986886e18d01139699261b3b2e5ded
Martin Di Paola <martinp.dipaola@gmail.com>
2018-12-15 18:42:56 +0000

    Zone finder based on Markdown Fenced Code blocks; disabled the Example Finder
    Mark the Markdown fenced-code blocks and the html comment blocks in a
    markdown file (.md) as valide zones to find examples.
    Disabled the MarkdownFencedCodeFinder example finder, no longer
    supported.

--- byexample/modules/markdown.py
+++ byexample/modules/markdown.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
 import re
-from byexample.finder import ExampleFinder
+from byexample.finder import ExampleFinder, ZoneFinder
 
 stability = 'stable'
 
-class FencedMatchFinder(ExampleFinder):
-    target = 'markdown-fenced-code'
+class FencedMatchFinder: #(ExampleFinder):
+    #target = 'markdown-fenced-code'
     specific = False
 
     def example_regex(self):
@@ -47,3 +47,28 @@ class FencedMatchFinder(ExampleFinder):
     def spurious_endings(self):
         endings = ExampleFinder.spurious_endings(self)
         return endings - {'```', '~~~'}
+
+class MarkdownFencedCodeFinder(ZoneFinder):
+    target = {'.md'}
+
+    def zone_regex(self):
+        return re.compile(r'''
+            # Begin with a markdown fenced-code marker or a html comment marker
+            ^[ ]*
+                (?:
+                    (?P<marker>```(?:``)*(?=[^`]))  # fenced-code marker (backticks)
+                    | (?:<!--)              # or the html comment marker
+                )
+
+            # then, grab everything until the first end marker
+            (?P<zone>(?:.*?\n)*?)
+
+            # finally, the end marker
+            ^[ ]*
+                (?(marker)    # if we matched a fenced-code maker previously
+                  (?P=marker) # then we must match the same amount of backticks
+                  |(?:-->)    # otherwise, we must match the close of the html comment
+                )
+                [ ]*$\n?
+            ''', re.MULTILINE | re.VERBOSE)
+
