1604766c1c6a8e050e4225a2965fc06e9459b49d
Martin Di Paola <martinp.dipaola@gmail.com>
2018-12-22 20:11:50 +0000

    Bump version to 8.0.0-a2 (alpha pre-release)

--- byexample/__init__.py
+++ byexample/__init__.py
@@ -1,6 +1,6 @@
 '''Write snippets of code in C++, Python, Ruby, and others as documentation and execute them as regression tests.'''
 
-__version__ = "8.0.0-a1"
+__version__ = "8.0.0-a2"
 
 _author  = 'Di Paola Martin'
 _license = 'GNU GPLv3'
