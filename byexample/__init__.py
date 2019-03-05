f0b9a2d8c14955536bfae9a439ad133d7ea5a60a
Martin Di Paola <martinp.dipaola@gmail.com>
2019-03-05 15:22:54 +0000

    Bump to version 8.1.0

--- byexample/__init__.py
+++ byexample/__init__.py
@@ -1,6 +1,6 @@
 '''Write snippets of code in C++, Python, Ruby, and others as documentation and execute them as regression tests.'''
 
-__version__ = "8.0.1"
+__version__ = "8.1.0"
 
 _author  = 'Di Paola Martin'
 _license = 'GNU GPLv3'
