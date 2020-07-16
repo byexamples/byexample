696a69bf2495c558fb0b9debbd71cfa8d098208a
Martin Di Paola <martinp.dipaola@gmail.com>
2020-07-16 17:54:38 +0000

    Bump version to 9.2.2

--- byexample/__init__.py
+++ byexample/__init__.py
@@ -1,6 +1,6 @@
 '''Write snippets of code in C++, Python, Ruby, and others as documentation and execute them as regression tests.'''
 
-__version__ = "9.2.1"
+__version__ = "9.2.2"
 
 _author = 'Di Paola Martin'
 _license = 'GNU GPLv3'
