017141a1c2fb4d2b7faf1117668924abe0011499
Martin Di Paola <martinp.dipaola@gmail.com>
2022-06-13 20:10:04 -0300

    Bump to 10.5.5

--- byexample/__init__.py
+++ byexample/__init__.py
@@ -1,6 +1,6 @@
 '''Write snippets of code in C++, Python, Ruby, and others as documentation and execute them as regression tests.'''
 
-__version__ = "10.5.4"
+__version__ = "10.5.5"
 
 _author = 'Di Paola Martin'
 _license = 'GNU GPLv3'
